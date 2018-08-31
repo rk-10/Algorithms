d1cee618e99449f24e847e9a5e2989b4586bce79
rk-10 <khanejarohan07@gmail.com>
2018-08-31 14:14:05 +0530

    adds insertion sort

---
+++ Algorithms/insertionSort.cpp
@@ -0,0 +1,42 @@
+//
+//  insertionSort.cpp
+//  Algorithms
+//
+//  Created by Rohan Khaneja on 30/08/18.
+//  Copyright © 2018 Rohan Khaneja. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+void insertionSort(int arr[], int size) {
+    for(int i = 0; i < size; i++){
+        std::cout << arr[i];
+    }
+    
+    int key, j;
+    for(int i = 1; i < size; i++){
+        key = arr[i];
+        j = i - 1;
+        //inserting the key
+        while(j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    
+    std::cout << "\n";
+    
+    for(int i = 0; i < size; i++){
+        std::cout << arr[i];
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    int ar[] = {1,7,2,5,4,3,6};
+    insertionSort(ar, 7);
+    return 0;
+}
+
+
